f2a87962e5c0b3af6d8ed96c9c165c87e7d837fc
garkusha <garkusha.constantine@gmail.com>
2015-04-10 13:11:39 +0200

    added icon

--- src/cz/garkusha/jobstack/util/Path.java
+++ src/cz/garkusha/jobstack/util/Path.java
@@ -22,13 +22,13 @@ public class Path {
     private static final String DATA_FOLDER = "data";
     private static final String DB_NAME = "seekingJob2015";
     private static final String JOBS_DESCRIPTION_FOLDER = "jobsDescription";
-    private static final String RESOURCES_FOLDER = "resources";
+    public static final String RESOURCES_FOLDER = "resources";
 
     private static final String PATH_TO_DB_FILE = "jdbc:derby:" + ABSOLUTE_PROGRAM_PATH + File.separator + DATA_FOLDER + File.separator + DB_NAME;
 
     private static final String RELATIVE_JOB_DESCRIPTION_PATH = DATA_FOLDER + File.separator + DB_NAME + File.separator + JOBS_DESCRIPTION_FOLDER + File.separator ;
     private static final String ABSOLUTE_JOB_DESCRIPTION_PATH = ABSOLUTE_PROGRAM_PATH + File.separator + RELATIVE_JOB_DESCRIPTION_PATH;
-    private static final String ABSOLUTE_RESOURCES_PATH = ABSOLUTE_PROGRAM_PATH + File.separator + DATA_FOLDER + File.separator + RESOURCES_FOLDER;
+    private static final String ABSOLUTE_RESOURCES_PATH = ABSOLUTE_PROGRAM_PATH + File.separator + DATA_FOLDER + File.separator + RESOURCES_FOLDER + File.separator ;
 
     public static String getAbsoluteResourcesPath() {
         return ABSOLUTE_RESOURCES_PATH;
